4c0f6a2ffba2f27dd5f28a81f8af5301f2d571e2
valerynaterchocho@gmail.com <valerynaterchocho@gmail.com>
2025-05-19 23:48:33 -0300

    Se agrega los niveles, se agrega el panel de victoria y se pasa de nivel o se resetea el mismo

---
+++ main/levels.lua
@@ -0,0 +1,160 @@
+-- levels.lua (definición de niveles personalizados)
+
+return {
+    [1] = {
+        grid = {
+            { "bear_blue", "bear_blue", "bear_blue", "bear_blue", "bear_blue" },
+            { "bear_blue", "bear_blue", "bear_blue", "bear_blue", "bear_blue" },
+            { "bear_blue", "bear_blue", "bear_blue", "bear_blue", "bear_blue" },
+            { "bear_blue", "bear_blue", "bear_blue", "bear_blue", "bear_blue" },
+            { "bear_blue", "bear_blue", "bear_blue", "bear_blue", "bear_blue" },
+            { "bear_blue", "bear_blue", "bear_blue", "bear_blue", "bear_blue" },
+        },
+        goals = {
+            { type = "bear_blue", required = 1 },
+        }
+    },
+
+    [2] = {
+        grid = {
+            { "bear_blue", "bear_pink", "bear_blue", "bear_pink", "bear_blue" },
+            { "bear_pink", "bear_blue", "bear_pink", "bear_blue", "bear_pink" },
+            { "bear_blue", "bear_pink", "bear_blue", "bear_pink", "bear_blue" },
+            { "bear_pink", "bear_blue", "bear_pink", "bear_blue", "bear_pink" },
+            { "bear_blue", "bear_pink", "bear_blue", "bear_pink", "bear_blue" },
+            { "bear_pink", "bear_blue", "bear_pink", "bear_blue", "bear_pink" },
+        },
+        goals = {
+            { type = "bear_blue", required = 2 },
+            { type = "bear_pink", required = 1 },
+        }
+    },
+
+    [3] = {
+        grid = {
+            { "bear_blue", "bear_pink", "candy",     "flip_flop",   "ball"     },
+            { "bear_pink", "flip_flop", "ball",      "flip_flop",   "bear_blue" },
+            { "ball",      "flip_flop", "bear_blue", "bear_pink",   "flip_flop" },
+            { "flip_flop", "ball",      "flip_flop", "bear_blue",   "candy"     },
+            { "bear_blue", "flip_flop", "ball",      "candy",       "bear_pink" },
+            { "flip_flop", "bear_blue", "bear_pink", "ball",        "flip_flop" },
+        },
+        goals = {
+            { type = "bear_blue", required = 4 },
+            { type = "bear_pink", required = 3 },
+            { type = "candy", required = 2 },
+        }
+    },
+
+    [4] = {
+        grid = {
+            { "flip_flop", "flip_flop", "ball", "candy", "candy" },
+            { "candy", "flip_flop", "bear_blue", "bear_pink", "flip_flop" },
+            { "bear_blue", "candy", "bear_pink", "flip_flop", "candy" },
+            { "bear_pink", "flip_flop", "bear_blue", "ball", "flip_flop" },
+            { "flip_flop", "candy", "flip_flop", "bear_pink", "bear_blue" },
+            { "bear_blue", "ball", "candy", "flip_flop", "candy" },
+        },
+        goals = {
+            { type = "bear_blue", required = 5 },
+            { type = "bear_pink", required = 4 },
+            { type = "flip_flop", required = 4 },
+        }
+    },
+
+    [5] = {
+        grid = {
+            { "bomb", "flip_flop", "ball", "flip_flop", "bomb" },
+            { "bear_blue", "flip_flop", "ball", "flip_flop", "bear_blue" },
+            { "candy", "flip_flop", "bear_blue", "bear_pink", "ball" },
+            { "flip_flop", "bomb", "flip_flop", "bear_blue", "flip_flop" },
+            { "bear_blue", "candy", "bomb", "ball", "bear_pink" },
+            { "flip_flop", "bomb", "bear_pink", "bear_blue", "flip_flop" },
+        },
+        goals = {
+            { type = "bear_blue", required = 5 },
+            { type = "flip_flop", required = 5 },
+            { type = "candy", required = 2 },
+        }
+    },
+
+    [6] = {
+        grid = {
+            { "bear_blue", "ball", "flip_flop", "candy", "bear_pink" },
+            { "flip_flop", "flip_flop", "ball", "ball", "flip_flop" },
+            { "bear_blue", "ball", "bear_blue", "flip_flop", "candy" },
+            { "flip_flop", "bear_pink", "ball", "candy", "bear_pink" },
+            { "candy", "flip_flop", "bear_blue", "flip_flop", "ball" },
+            { "bear_blue", "bear_pink", "ball", "flip_flop", "candy" },
+        },
+        goals = {
+            { type = "bear_blue", required = 5 },
+            { type = "bear_pink", required = 4 },
+            { type = "flip_flop", required = 6 },
+        }
+    },
+
+    [7] = {
+        grid = {
+            { "candy", "candy", "candy", "ball", "ball" },
+            { "flip_flop", "flip_flop", "flip_flop", "flip_flop", "flip_flop" },
+            { "bear_blue", "bear_pink", "bear_blue", "bear_pink", "bear_blue" },
+            { "ball", "flip_flop", "candy", "flip_flop", "ball" },
+            { "flip_flop", "bear_blue", "bear_pink", "candy", "flip_flop" },
+            { "bear_blue", "candy", "ball", "flip_flop", "bear_pink" },
+        },
+        goals = {
+            { type = "bear_blue", required = 6 },
+            { type = "flip_flop", required = 6 },
+            { type = "candy", required = 6 },
+        }
+    },
+
+    [8] = {
+        grid = {
+            { "flip_flop", "ball", "bear_pink", "bear_blue", "flip_flop" },
+            { "ball", "ball", "flip_flop", "ball", "ball" },
+            { "bear_pink", "bear_pink", "bear_blue", "bear_pink", "bear_blue" },
+            { "candy", "candy", "flip_flop", "candy", "candy" },
+            { "bear_blue", "bear_pink", "flip_flop", "ball", "bear_blue" },
+            { "flip_flop", "candy", "bear_pink", "flip_flop", "ball" },
+        },
+        goals = {
+            { type = "bear_pink", required = 5 },
+            { type = "bear_blue", required = 5 },
+            { type = "ball", required = 5 },
+        }
+    },
+
+    [9] = {
+        grid = {
+            { "ball", "flip_flop", "flip_flop", "flip_flop", "ball" },
+            { "candy", "candy", "candy", "candy", "candy" },
+            { "bear_blue", "bear_pink", "bear_blue", "bear_pink", "bear_blue" },
+            { "flip_flop", "ball", "candy", "ball", "flip_flop" },
+            { "bear_pink", "bear_blue", "bear_pink", "bear_blue", "bear_pink" },
+            { "candy", "flip_flop", "bear_blue", "flip_flop", "ball" },
+        },
+        goals = {
+            { type = "candy", required = 7 },
+            { type = "flip_flop", required = 6 },
+            { type = "bear_blue", required = 5 },
+        }
+    },
+
+    [10] = {
+        grid = {
+            { "bug", "bear_blue", "flip_flop", "ball", "bear_pink" },
+            { "bear_blue", "bug", "candy", "bug", "bear_blue" },
+            { "flip_flop", "ball", "bear_blue", "bear_pink", "candy" },
+            { "ball", "bug", "flip_flop", "bear_blue", "ball" },
+            { "bear_blue", "candy", "bug", "ball", "bear_pink" },
+            { "ball", "bug", "bear_pink", "bear_blue", "flip_flop" },
+        },
+        goals = {
+            { type = "bear_blue", required = 6 },
+            { type = "bug", required = 2 },
+            { type = "ball", required = 4 },
+        }
+    }
+}
